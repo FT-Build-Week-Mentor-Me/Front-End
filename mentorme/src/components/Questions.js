855be924624ffbcb65c4e2933d1e8656dd412aec
Aasa-Christian <aasachristian@yahoo.com>
2019-11-18 16:05:26 -0500

    created questions component

---
+++ mentorme/src/components/Questions.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Questions = props => {
+
+    const handleChange = e =>{
+        props({...props, [e.target.name]: e.target.value});
+    }
+
+
+    return(
+        <div>
+                <form onSubmit={props}>
+                    <label htmlFor="title"></label>
+                        <input
+                        type="text"
+                        name="title"
+                        id="title"
+                         onChange={handleChange}
+                        // value={props.title}
+                        />
+                    <label htmlFor="question"></label>
+                        <input
+                        type ="textarea"
+                        name="question"
+                        id="question"
+                         onChange={handleChange}
+                        // value={props.question}
+                        />
+
+
+                    <label htmlFor="category"></label>
+                        <select
+                        name="category"
+                        id="category"
+                         onChange={handleChange}
+                        // value={props.category}
+                        >
+                            <option value="option">option</option>
+                            <option value="option">option</option> 
+                            <option value="option">option</option> 
+                            <option value="option">option</option> 
+                        </select>
+                </form>
+            </div>
+    )
+    
+}
+export default Questions;
