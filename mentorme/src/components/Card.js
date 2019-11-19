f90543e4ad9d562ce605e9803a06c00cfec4067c
Aasa-Christian <aasachristian@yahoo.com>
2019-11-19 13:56:11 -0500

    added gsp with animation on card component

--- mentorme/src/components/Card.js
+++ mentorme/src/components/Card.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React,{useRef,useEffect} from "react";
+import {TweenMax, Power3 } from "gsap/all"; 
 
 const Card = props => {
+    let cardContent = useRef(null)
 
+    useEffect(() => {
+        TweenMax.to(
+            cardContent,
+            .8, {opacity: 100, y:20, ease: Power3.easeOut}
+        )
+    },[])
 
-
+    
     return(
 
-        <div className="card">
+        <div className="card"  ref={el => cardContent = el}>
 
-            <section key={props.threadId}>
+            <section key={props.threadId} >
                 <h3>{props.title}</h3>
                 <p>{props.content}</p>
             </section>
